d43d78da1d334a770c3fc90c3781359573a70eee
blkfinch <blkfinch@gmail.com>
2019-11-29 14:29:24 -0700

    added jokes

--- discord/chillbot.rb
+++ discord/chillbot.rb
@@ -1,4 +1,5 @@
 require 'discordrb'
+require 'rest-client'
 
 file = File.read('secrets.json')
 SECRETS = JSON.parse(file)
@@ -10,8 +11,17 @@ bot.message(with_text: 'Ping!') do |event|
   event.respond 'Bong!'
 end
 
+bot.message(with_text: 'joke') do |event|
+  response = JSON.parse(RestClient.get "http://jokes.guyliangilsing.me/retrieveJokes.php?type=dadjoke")
+  event.respond response["joke"]
+end
+
 bot.mention() do |event|
   event.respond 'I heard my name'
+end
+
+bot.presence do |event|
+  event.server.general_channel.send_message "#{event.user.name} has become #{event.user.status}!"
 end
 
 bot.run
